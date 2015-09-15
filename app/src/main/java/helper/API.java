56f7255064533d5b7be3936671267987019aaad0
Amir Fazwan <amirfazwan91@gmail.com>
2015-09-15 09:46:01 +0800

    change api

--- app/src/main/java/helper/API.java
+++ app/src/main/java/helper/API.java
@@ -11,7 +11,11 @@ public class API {
     public static String API = "API/";
     public static String FILE_PATH = "api.php?";
     public static String URL = HOST + DIRECTORY + API + FILE_PATH;
-    public static String STATISTIC_2015 = "statistic_geocode_2015.json";
-    public static String URL_STATISTICS =  HOST + DIRECTORY + STATISTIC_2015;
+    //public static String STATISTIC_2015 = "statistic_geocode_2015.json";
+    //http://dev.rootmybox.com/qittoalert/API/api.php?getData&tahun=2015
+    //public static String URL_STATISTICS =  HOST + DIRECTORY + STATISTIC_2015;
+    public static String FORECAST = "http://api.openweathermap.org/data/2.5/forecast?q=kajang,my&mode=json&units=metric&cnt=1";
+    public static String WEATHER_ICON = "http://openweathermap.org/img/w/";
+    public static String ICON_EXT = ".png";
 
 }
